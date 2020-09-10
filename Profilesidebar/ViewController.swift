82b23a7d505535619e35fdea5dfa1a9338f47eda
ABid A.Sobhan <abida.sobhan@ABids-iMac.local>
2020-09-10 04:07:55 -0700

    Initial Commit

---
+++ Profilesidebar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Profilesidebar
+//
+//  Created by ABid A.Sobhan on 10/9/20.
+//  Copyright © 2020 ABid A.Sobhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
